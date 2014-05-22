78f442ff091691b84a9b2c33c666823736163a6d
Idoia <idoia.murua@tecnalia.com>
2014-05-22 15:27:14 +0200

    HelloWorld class to test Eclipse/git/maven

---
+++ aliada/aliada-links-discovery/src/main/java/eu/aliada/linksDiscovery/HellowWorld.java
@@ -0,0 +1,25 @@
+// ALIADA - Automatic publication under Linked Data paradigm
+//          of library and museum data
+//
+// Component: aliada-links-discovery
+// Responsible: Tecnalia
+// Version: 0.1
+package eu.aliada.linksDiscovery;
+
+/**
+ * Java program for Git and Maven testing purposes.
+ * @author Idoia Murua
+ * @version 0.1
+ */
+public class HellowWorld {
+
+	/** main method.
+	 *
+	 * @param args		Input parameter
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
