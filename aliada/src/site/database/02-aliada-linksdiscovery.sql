be862da47a371340ed8250d8c5fc03ea55c06d04
Idoia <idoia.murua@tecnalia.com>
2014-07-02 11:17:20 +0200

    Corrected keys and foreign keys.

--- aliada/src/site/database/02-aliada-linksdiscovery.sql
+++ aliada/src/site/database/02-aliada-linksdiscovery.sql
@@ -20,8 +20,8 @@ CREATE TABLE `aliada`.`linksdiscovery_subjob_instances` (
 `subjob_id` INT NOT NULL ,
 `start_date` DATETIME default NULL,
 `end_date` DATETIME default NULL ,
-PRIMARY KEY ( `job_id` )
+PRIMARY KEY ( `job_id` , `subjob_id`)
 ) ENGINE = InnoDB ;
 
 ALTER TABLE  `aliada`.`linksdiscovery_subjob_instances`
-  ADD CONSTRAINT `linksdiscovery_subjob_instances_ibfk_1` FOREIGN KEY (job_id`) REFERENCES `aliada`.`linksdiscovery_job_instances` (`job_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `linksdiscovery_subjob_instances_ibfk_1` FOREIGN KEY (`job_id`) REFERENCES `aliada`.`linksdiscovery_job_instances` (`job_id`) ON DELETE CASCADE ON UPDATE CASCADE;
