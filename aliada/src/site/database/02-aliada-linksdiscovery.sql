4b4b0d79894b447f45b41ccdcf495482a9d5d7d6
107126 <107126@PC-IMURUA-INF.tri.lan>
2014-07-02 10:01:16 +0200

    Inserted new table for links discovery subjobs.

--- aliada/src/site/database/02-aliada-linksdiscovery.sql
+++ aliada/src/site/database/02-aliada-linksdiscovery.sql
@@ -8,7 +8,20 @@ CREATE TABLE `aliada`.`linksdiscovery_job_instances` (
 `output_login`  VARCHAR( 32 ) default NULL,
 `output_password`  VARCHAR( 32 ) default NULL,
 `output_graph`  VARCHAR( 245 ) default NULL,
+`config_file`  VARCHAR( 245 ) default NULL,
+`tmp_dir`  VARCHAR( 245 ) default NULL,
 `start_date` DATETIME default NULL,
 `end_date` DATETIME default NULL ,
 PRIMARY KEY ( `job_id` )
 ) ENGINE = InnoDB ;
+
+CREATE TABLE `aliada`.`linksdiscovery_subjob_instances` (
+`job_id` INT NOT NULL ,
+`subjob_id` INT NOT NULL ,
+`start_date` DATETIME default NULL,
+`end_date` DATETIME default NULL ,
+PRIMARY KEY ( `job_id` )
+) ENGINE = InnoDB ;
+
+ALTER TABLE  `aliada`.`linksdiscovery_subjob_instances`
+  ADD CONSTRAINT `linksdiscovery_subjob_instances_ibfk_1` FOREIGN KEY (job_id`) REFERENCES `aliada`.`linksdiscovery_job_instances` (`job_id`) ON DELETE CASCADE ON UPDATE CASCADE;
