5f9dab024292cb7f94e38883bf0d6f971bae2240
agazzarini <agazzarini@andrea-desktop>
2014-09-03 16:17:59 +0200

    [ issue #38 ] Fix on RDfizer job instances table

--- aliada/src/site/database/01-aliada-rdfizer.sql
+++ aliada/src/site/database/01-aliada-rdfizer.sql
@@ -1,17 +1,17 @@
-CREATE TABLE `aliada`.`rdfizer_job_instances` (
-`job_id` INT NOT NULL AUTO_INCREMENT,
-`datafile`  VARCHAR( 245 ) NOT NULL,
-`format`  VARCHAR( 32 ) NOT NULL,
-`namespace`  VARCHAR( 245 ) NOT NULL,
-`graph_name`  VARCHAR( 245 ) default NULL,
-`aliada_ontology`  VARCHAR( 245 ) NOT NULL,
-`start_date` TIMESTAMP,
-`end_date` TIMESTAMP,
-`sparql_endpoint_uri`  VARCHAR( 245 ) NOT NULL, 
-`sparql_endpoint_login`  VARCHAR( 32 ) NOT NULL, 
-`sparql_endpoint_password`  VARCHAR( 32 ) NOT NULL,
-PRIMARY KEY ( `job_id` )
-) ENGINE = InnoDB ;
+CREATE TABLE `rdfizer_job_instances` (
+  `job_id` int(11) NOT NULL AUTO_INCREMENT,
+  `datafile` varchar(245) NOT NULL,
+  `format` varchar(32) NOT NULL,
+  `namespace` varchar(245) NOT NULL,
+  `graph_name` varchar(245) DEFAULT NULL,
+  `aliada_ontology` varchar(245) NOT NULL,
+  `start_date` timestamp NULL DEFAULT NULL,
+  `end_date` timestamp NULL DEFAULT NULL,
+  `sparql_endpoint_uri` varchar(245) NOT NULL,
+  `sparql_endpoint_login` varchar(32) NOT NULL,
+  `sparql_endpoint_password` varchar(32) NOT NULL,
+  PRIMARY KEY (`job_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `aliada`.`rdfizer_job_stats` (
 `job_id` INT NOT NULL ,
