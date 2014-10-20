09b70de90a22e77611fcb8553fadc6eaee175d6c
idoiamurua <idoia.murua@tecnalia.com>
2014-10-20 16:14:59 +0200

    Debugging traces added.

--- aliada/aliada-links-discovery/src/main/java/eu/aliada/linksdiscovery/impl/LinksDiscovery.java
+++ aliada/aliada-links-discovery/src/main/java/eu/aliada/linksdiscovery/impl/LinksDiscovery.java
@@ -514,7 +514,25 @@ public class LinksDiscovery {
 			final String command = "crontab " + crontabFilename;
 			try {
 				LOGGER.debug(MessageCatalog._00040_EXECUTING_CRONTAB);
-				Runtime.getRuntime().exec(command);
+				Process p = Runtime.getRuntime().exec(command);
+				String s = null;
+				BufferedReader stdInput = new BufferedReader(new
+		        InputStreamReader(p.getInputStream()));
+		 
+		        BufferedReader stdError = new BufferedReader(new
+		        InputStreamReader(p.getErrorStream()));
+		 
+		        // read the output from the command
+		        System.out.println("Here is the standard output of the command:\n");
+		        while ((s = stdInput.readLine()) != null) {
+		        	System.out.println(s);
+		        }
+		             
+		        // read any errors from the attempted command
+		        System.out.println("Here is the standard error of the command (if any):\n");
+		        while ((s = stdError.readLine()) != null) {
+		        	System.out.println(s);
+		        }
 			} catch (IOException exception) {
 				LOGGER.error(MessageCatalog._00033_EXTERNAL_PROCESS_START_FAILURE, exception, command);
 			}
