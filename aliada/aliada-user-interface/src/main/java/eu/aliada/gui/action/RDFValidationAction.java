f203c9dbb27eb90b49ca08f04b34de0a29b8e50a
iosa <iosa@port_elena.scanbit>
2014-10-21 10:02:25 +0200

    Avoid NPE in the RDF validation action

--- aliada/aliada-user-interface/src/main/java/eu/aliada/gui/action/RDFValidationAction.java
+++ aliada/aliada-user-interface/src/main/java/eu/aliada/gui/action/RDFValidationAction.java
@@ -31,26 +31,31 @@ public class RDFValidationAction extends ActionSupport {
     private final Log logger = new Log(InstitutionConfigurationAction.class);
     
     public String execute(){
-        FileWork importedFile = (FileWork) ServletActionContext.getRequest().getSession().getAttribute("importedFile"); 
-        Connection connection;
-        connection = new DBConnectionManager().getConnection();
-        Statement statement;
-        try {
-            statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT public_sparql_endpoint_uri, graph_uri FROM organisation o INNER JOIN graph g ON o.organisationId=g.organisationId WHERE graph_uri='"+importedFile.getGraph()+"'");
-            if (rs.next() ) {
-                setSparqlEndpoint(rs.getString("public_sparql_endpoint_uri"));
-                setGraphUri(rs.getString("graph_uri"));
+        if(ServletActionContext.getRequest().getSession().getAttribute("importedFile")!=null){
+            FileWork importedFile = (FileWork) ServletActionContext.getRequest().getSession().getAttribute("importedFile"); 
+            Connection connection;
+            connection = new DBConnectionManager().getConnection();
+            Statement statement;
+            try {
+                statement = connection.createStatement();
+                ResultSet rs = statement.executeQuery("SELECT public_sparql_endpoint_uri, graph_uri FROM organisation o INNER JOIN graph g ON o.organisationId=g.organisationId WHERE graph_uri='"+importedFile.getGraph()+"'");
+                if (rs.next() ) {
+                    setSparqlEndpoint(rs.getString("public_sparql_endpoint_uri"));
+                    setGraphUri(rs.getString("graph_uri"));
+                }
+                rs.close();
+                statement.close();
+                connection.close();
+                return SUCCESS;
+            } catch (SQLException e) {
+                logger.error(MessageCatalog._00011_SQL_EXCEPTION,e);
+                return ERROR;
             }
-            rs.close();
-            statement.close();
-            connection.close();
-            return SUCCESS;
-        } catch (SQLException e) {
-            logger.error(MessageCatalog._00011_SQL_EXCEPTION,e);
-            return ERROR;
         }
-        
+        else{
+            logger.error(MessageCatalog._00033_CONVERSION_ERROR_NO_FILE_IMPORTED);
+            return ERROR;
+        }        
     }
     
     /**
