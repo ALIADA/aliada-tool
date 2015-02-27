8ebead31123b8be06bca332c870d2fcdd9ce22e6
idoiamurua <idoia.murua@tecnalia.com>
2015-02-27 14:16:11 +0100

    New ISQL command files.

--- aliada/aliada-linked-data-server/src/main/resources/isql_rewrite_rules_global.sql
+++ aliada/aliada-linked-data-server/src/main/resources/isql_rewrite_rules_global.sql
@@ -14,6 +14,10 @@
 --$u{dataset_page}: dataset_page, e.g.: dataset.html
 --$u{aliada_ont_encoded}: URL encoded aliada_uri, e.g.: http://aliada-project.eu/2014/aliada-ontology# URL encoded
 --$u{uri_id_encoded}: URL encoded uri_id_part, e.g.: id%2Fresource
+--$u{create_virtual_path}: specifies whether it is required to create a new virtual path for the DOC listing with extension (1 or 0).
+--$u{urrl_list_subset}: specifies whether it refers to the global dataset or a subset (1 or 0).
+--$u{rules_suffix_dataset}: rulesNamesSuffix for the global dataset, e.g.: dataartiumorg
+--$u{uri_doc_concept_parent}: parent path of uri_doc_part + dataset.uri_concept_part, e.g.: '' for 'doc', 'doc' for 'doc/collections'
 ----------------------------------------------------------------
 
 ----------------------------------------------------------------
@@ -33,9 +37,55 @@ DB.DBA.VHOST_DEFINE (
 	 is_dav=>0,
 	 def_page=>'$u{dataset_page}',
 	 ses_vars=>0,
-	 opts=>vector ('browse_sheet', ''),
+	 opts=>vector ('browse_sheet', '', 'url_rewrite', 'http_rule_list_$u{rules_suffix}_Root'),
 	 is_default_host=>0
 );
+DB.DBA.URLREWRITE_CREATE_RULELIST ( 
+'http_rule_list_$u{rules_suffix}_Root', 1, 
+  vector ('http_rule_$u{rules_suffix}_Ont_extension_n3', 'http_rule_$u{rules_suffix}_Ont_extension_xml', 'http_rule_$u{rules_suffix}_Ont_extension_json'));
+
+DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
+'http_rule_$u{rules_suffix}_Ont_extension_n3', 1, 
+'/$u{uri_def}(.*)\\.(ttl)', 
+vector ('par_1', 'par_2'),
+2, 
+'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20DESCRIBE%%20%%3Fa%%20FROM%%20%%3C$u{aliada_ont_encoded}%%3E%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=n3', 
+vector ('par_1'), 
+NULL, 
+NULL, 
+2, 
+0, 
+'' 
+);
+
+DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
+'http_rule_$u{rules_suffix}_Ont_extension_xml', 1, 
+'/$u{uri_def}(.*)\\.(rdf)', 
+vector ('par_1', 'par_2'),
+2, 
+'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20DESCRIBE%%20%%3Fa%%20FROM%%20%%3C$u{aliada_ont_encoded}%%3E%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=rdf', 
+vector ('par_1'), 
+NULL, 
+NULL, 
+2, 
+0, 
+'' 
+);
+
+DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
+'http_rule_$u{rules_suffix}_Ont_extension_json', 1, 
+'/$u{uri_def}(.*)\\.(json)', 
+vector ('par_1', 'par_2'),
+2, 
+'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20DESCRIBE%%20%%3Fa%%20FROM%%20%%3C$u{aliada_ont_encoded}%%3E%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&output=application%%2Frdf%%2Bjson', 
+vector ('par_1'), 
+NULL, 
+NULL, 
+2, 
+0, 
+'' 
+);
+  
 ----------------------------------------------------------------
 --Identifier URI : With 303 redirects
 ----------------------------------------------------------------
@@ -48,27 +98,27 @@ DB.DBA.VHOST_REMOVE (
 DB.DBA.VHOST_DEFINE (
 	 lhost=>'$u{lhost}',
 	 vhost=>'$u{vhost}',
-	 lpath=>'/$u{uri_id},
+	 lpath=>'/$u{uri_id}',
 	 ppath=>'/DAV/home/$u{uri_id}/',
 	 is_dav=>1,
 	 def_page=>'',
 	 vsp_user=>'dba',
 	 ses_vars=>0,
-	 opts=>vector ('browse_sheet', '', 'url_rewrite', 'http_rule_list_$u{rules_suffix}_1'),
+	 opts=>vector ('browse_sheet', '', 'url_rewrite', 'http_rule_list_$u{rules_suffix}_Id'),
 	 is_default_host=>0
 );
 
 DB.DBA.URLREWRITE_CREATE_RULELIST ( 
-'http_rule_list_$u{rules_suffix}_1', 1, 
-  vector ('http_rule_$u{rules_suffix}_1_n3', 'http_rule_$u{rules_suffix}_1_xml', 'http_rule_$u{rules_suffix}_1_json', 'http_rule_$u{rules_suffix}_1_html', 'http_rule_$u{rules_suffix}_1_other'));
+'http_rule_list_$u{rules_suffix}_Id', 1, 
+  vector ('http_rule_$u{rules_suffix}_Id_n3', 'http_rule_$u{rules_suffix}_Id_xml', 'http_rule_$u{rules_suffix}_Id_json', 'http_rule_$u{rules_suffix}_Id_html', 'http_rule_$u{rules_suffix}_Id_other'));
 
 
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_1_n3', 1, 
+'http_rule_$u{rules_suffix}_Id_n3', 1, 
 '/$u{uri_id}(/[^#]*)', 
 vector ('par_1'), 
 1, 
-'/$u{uri_doc_concept}%U.ttl', 
+'/$u{uri_doc_concept}%s.ttl', 
 vector ('par_1', '*accept*'), 
 NULL, 
 '(text/rdf.n3)', 
@@ -78,11 +128,11 @@ NULL,
 );
 
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_1_xml', 1, 
+'http_rule_$u{rules_suffix}_Id_xml', 1, 
 '/$u{uri_id}(/[^#]*)', 
 vector ('par_1'), 
 1, 
-'/$u{uri_doc_concept}%U.rdf', 
+'/$u{uri_doc_concept}%s.rdf', 
 vector ('par_1', '*accept*'), 
 NULL, 
 '(application/rdf.xml)', 
@@ -92,11 +142,11 @@ NULL,
 );
 
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_1_json', 1, 
+'http_rule_$u{rules_suffix}_Id_json', 1, 
 '/$u{uri_id}(/[^#]*)', 
 vector ('par_1'), 
 1, 
-'/$u{uri_doc_concept}%U.json', 
+'/$u{uri_doc_concept}%s.json', 
 vector ('par_1', '*accept*'), 
 NULL, 
 '(application/rdf.json)', 
@@ -106,11 +156,11 @@ NULL,
 );
 
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_1_html', 1, 
+'http_rule_$u{rules_suffix}_Id_html', 1, 
 '/$u{uri_id}(/[^#]*)', 
 vector ('par_1'), 
 1, 
-'/$u{uri_doc_concept}%U.html', 
+'/$u{uri_doc_concept}%s.html', 
 vector ('par_1', '*accept*'), 
 NULL, 
 '(text/html)', 
@@ -120,11 +170,11 @@ NULL,
 );
 
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_1_other', 1, 
+'http_rule_$u{rules_suffix}_Id_other', 1, 
 '/$u{uri_id}(/[^#]*)', 
 vector ('par_1'), 
 1, 
-'/$u{uri_doc_concept}%U', 
+'/$u{uri_doc_concept}%s', 
 vector ('par_1', '*accept*'), 
 NULL, 
 '(application/ld.json)|(text/plain)|(\\*/\\*)', 
@@ -145,28 +195,28 @@ DB.DBA.VHOST_REMOVE (
 DB.DBA.VHOST_DEFINE (
 	 lhost=>'$u{lhost}',
 	 vhost=>'$u{vhost}',
-	 lpath=>'/$u{uri_def},
+	 lpath=>'/$u{uri_def}',
 	 ppath=>'/DAV/home/$u{uri_def}/',
 	 is_dav=>1,
 	 def_page=>'',
 	 vsp_user=>'dba',
 	 ses_vars=>0,
-	 opts=>vector ('browse_sheet', '', 'url_rewrite', 'http_rule_list_$u{rules_suffix}_3'),
+	 opts=>vector ('browse_sheet', '', 'url_rewrite', 'http_rule_list_$u{rules_suffix}_Ont'),
 	 is_default_host=>0
 );
 
 DB.DBA.URLREWRITE_CREATE_RULELIST ( 
-'http_rule_list_$u{rules_suffix}_3', 1, 
-  vector ('http_rule_$u{rules_suffix}_3_no_extension_rdf', 'http_rule_$u{rules_suffix}_3_no_extension_others', 'http_rule_$u{rules_suffix}_3_extension_n3', 'http_rule_$u{rules_suffix}_3_extension_xml', 'http_rule_$u{rules_suffix}_3_extension_json'));
+'http_rule_list_$u{rules_suffix}_Ont', 1, 
+  vector ('http_rule_$u{rules_suffix}_Ont_no_extension_rdf', 'http_rule_$u{rules_suffix}_Ont_no_extension_others'));
 
   
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_3_no_extension_rdf', 1, 
-'/$u{uri_def}(/[^.]*)', 
+'http_rule_$u{rules_suffix}_Ont_no_extension_rdf', 1, 
+'/$u{uri_def}([^.]*)', 
 vector ('par_1'), 
 1, 
-'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20SELECT%%20%%2A%%20%%20FROM%%20%%3C$u{aliada_ont_encoded}%%3E%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=%U', 
-vector ('par_1', '*accept*'), 
+'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20DESCRIBE%%20%%3Fa%%20FROM%%20%%3C$u{aliada_ont_encoded}%%3E%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=%U', 
+vector ('*accept*'), 
 NULL, 
 '(text/rdf.n3)|(application/rdf.xml)|(application/rdf.json)|(application/ld.json)|(text/plain)', 
 2, 
@@ -175,59 +225,14 @@ NULL,
 );
  
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_3_no_extension_others', 1, 
-'/$u{uri_def}(/[^.]*)', 
+'http_rule_$u{rules_suffix}_Ont_no_extension_others', 1, 
+'/$u{uri_def}([^.]*)', 
 vector ('par_1'), 
 1, 
-'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20SELECT%%20%%2A%%20%%20FROM%%20%%3C$u{aliada_ont_encoded}%%3E%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=rdf', 
+'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20DESCRIBE%%20%%3Fa%%20FROM%%20%%3C$u{aliada_ont_encoded}%%3E%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=html', 
 vector ('par_1'), 
 NULL, 
 '(text/html)|(\\*/\\*)', 
-2, 
-303, 
-'' 
-);
-
-DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_3_extension_n3', 1, 
-'/$u{uri_def}(/.*)\\.(ttl)', 
-vector ('par_1', 'par_2'),
-2, 
-'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20SELECT%%20%%2A%%20%%20FROM%%20%%3C$u{aliada_ont_encoded}%%3E%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=n3', 
-', 
-vector ('par_1'), 
-NULL, 
-NULL, 
-2, 
-0, 
-'' 
-);
-
-DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_3_extension_xml', 1, 
-'/$u{uri_def}(/.*)\\.(rdf)', 
-vector ('par_1', 'par_2'),
-2, 
-'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20SELECT%%20%%2A%%20%%20FROM%%20%%3C$u{aliada_ont_encoded}%%3E%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=rdf', 
-, 
-vector ('par_1'), 
-NULL, 
-NULL, 
-2, 
-0, 
-'' 
-);
-
-DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_3_extension_json', 1, 
-'/$u{uri_def}(/.*)\\.(json)', 
-vector ('par_1', 'par_2'),
-2, 
-'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20SELECT%%20%%2A%%20%%20FROM%%20%%3C$u{aliada_ont_encoded}%%3E%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=json', 
-', 
-vector ('par_1'), 
-NULL, 
-NULL, 
 2, 
 0, 
 '' 
@@ -245,31 +250,30 @@ DB.DBA.VHOST_REMOVE (
 DB.DBA.VHOST_DEFINE (
 	 lhost=>'$u{lhost}',
 	 vhost=>'$u{vhost}',
-	 lpath=>'/$u{uri_doc_concept},
+	 lpath=>'/$u{uri_doc_concept}',
 	 ppath=>'/DAV/home/$u{uri_doc_concept}/',
 	 is_dav=>1,
 	 def_page=>'',
 	 vsp_user=>'dba',
 	 ses_vars=>0,
-	 opts=>vector ('browse_sheet', '', 'url_rewrite', 'http_rule_list_$u{rules_suffix}_2'),
+	 opts=>vector ('browse_sheet', '', 'url_rewrite', 'http_rule_list_$u{rules_suffix}_Doc'),
 	 is_default_host=>0
 );
 
 DB.DBA.URLREWRITE_CREATE_RULELIST ( 
-'http_rule_list_$u{rules_suffix}_2', 1, 
-  vector ('http_rule_$u{rules_suffix}_2_list_no_extension_rdf', 'http_rule_$u{rules_suffix}_2_list_no_extension_others', 'http_rule_$u{rules_suffix}_2_list_extension_list_n3', 'http_rule_$u{rules_suffix}_2_list_extension_xml', 'http_rule_$u{rules_suffix}_2_list_extension_json',
-  'http_rule_$u{rules_suffix}_2_no_extension_rdf', 'http_rule_$u{rules_suffix}_2_no_extension_htmlvirtuoso', 
-  'http_rule_$u{rules_suffix}_2_extension_n3', 'http_rule_$u{rules_suffix}_2_extension_xml', 'http_rule_$u{rules_suffix}_2_extension_json', 'http_rule_$u{rules_suffix}_2_extension_html', 'http_rule_$u{rules_suffix}_2_extension_opac'));
+'http_rule_list_$u{rules_suffix}_Doc', 1, 
+  vector ('http_rule_$u{rules_suffix}_Doc_list_no_extension_rdf', 'http_rule_$u{rules_suffix}_Doc_list_no_extension_others', 'http_rule_$u{rules_suffix}_Doc_no_extension_rdf', 'http_rule_$u{rules_suffix}_Doc_no_extension_htmlvirtuoso', 
+  'http_rule_$u{rules_suffix}_Doc_extension_n3', 'http_rule_$u{rules_suffix}_Doc_extension_xml', 'http_rule_$u{rules_suffix}_Doc_extension_json', 'http_rule_$u{rules_suffix}_Doc_extension_html', 'http_rule_$u{rules_suffix}_Doc_extension_opac'));
 
 	
 --List URI-s of a dataset
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_2_list_no_extension_rdf', 1, 
-'($u{uri_doc_concept})', 
+'http_rule_$u{rules_suffix}_Doc_list_no_extension_rdf', 1, 
+'/($u{uri_doc_concept})\$', 
 vector ('par_1'), 
 1, 
-'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20SELECT%%20%%2A%%20$u{graphs_select_encoded}%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=%U', 
-vector ('par_1', '*accept*'), 
+'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20DESCRIBE%%20%%3Fa%%20$u{graphs_select_encoded}%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=%U', 
+vector ('*accept*'), 
 NULL, 
 '(text/rdf.n3)|(application/rdf.xml)|(application/rdf.json)|(application/ld.json)|(text/plain)', 
 1, 
@@ -278,26 +282,25 @@ NULL,
 );
  
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_2_list_no_extension_others', 1, 
-'($u{uri_doc_concept})', 
+'http_rule_$u{rules_suffix}_Doc_list_no_extension_others', 1, 
+'/($u{uri_doc_concept})\$', 
 vector ('par_1'), 
 1, 
-'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20SELECT%%20%%2A%%20$u{graphs_select_encoded}%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=rdf', 
+'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20DESCRIBE%%20%%3Fa%%20$u{graphs_select_encoded}%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=html', 
 vector ('par_1'), 
 NULL, 
 '(text/html)|(\\*/\\*)', 
 1, 
-303, 
+0, 
 '' 
 );
 
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_2_list_extension_list_n3', 1, 
-'($u{uri_doc_concept})\\.(ttl)', 
+'http_rule_$u{rules_suffix}_Doc_list_extension_n3', 1, 
+'/($u{uri_doc_concept})\\.(ttl)', 
 vector ('par_1', 'par_2'),
 2, 
-'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20SELECT%%20%%2A%%20$u{graphs_select_encoded}%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=n3', 
-', 
+'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20DESCRIBE%%20%%3Fa%%20$u{graphs_select_encoded}%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=n3', 
 vector ('par_1'), 
 NULL, 
 NULL, 
@@ -307,12 +310,11 @@ NULL,
 );
 
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_2_list_extension_xml', 1, 
-'($u{uri_doc_concept})\\.(rdf)', 
+'http_rule_$u{rules_suffix}_Doc_list_extension_xml', 1, 
+'/($u{uri_doc_concept})\\.(rdf)', 
 vector ('par_1', 'par_2'),
 2, 
-'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20SELECT%%20%%2A%%20$u{graphs_select_encoded}%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=rdf', 
-, 
+'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20DESCRIBE%%20%%3Fa%%20$u{graphs_select_encoded}%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=rdf', 
 vector ('par_1'), 
 NULL, 
 NULL, 
@@ -322,12 +324,11 @@ NULL,
 );
 
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_2_list_extension_json', 1, 
-'($u{uri_doc_concept})\\.(json)', 
+'http_rule_$u{rules_suffix}_Doc_list_extension_json', 1, 
+'/($u{uri_doc_concept})\\.(json)', 
 vector ('par_1', 'par_2'),
 2, 
-'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20SELECT%%20%%2A%%20$u{graphs_select_encoded}%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&format=json', 
-', 
+'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20DESCRIBE%%20%%3Fa%%20$u{graphs_select_encoded}%%20WHERE%%20%%7B%%3Fa%%20%%3Fb%%20%%3Fc%%7D&output=application%%2Frdf%%2Bjson', 
 vector ('par_1'), 
 NULL, 
 NULL, 
@@ -338,7 +339,7 @@ NULL,
 
 --Doc URI-s	
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_2_no_extension_rdf', 1, 
+'http_rule_$u{rules_suffix}_Doc_no_extension_rdf', 1, 
 '($u{uri_doc_slash})?(/[^.#]*)', 
 vector ('par_1', 'par_2'),  
 2, 
@@ -352,7 +353,7 @@ NULL,
 );
  
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_2_no_extension_htmlvirtuoso', 1, 
+'http_rule_$u{rules_suffix}_Doc_no_extension_htmlvirtuoso', 1, 
 '($u{uri_doc_slash})?(/[^.#]*)', 
 vector ('par_1', 'par_2'),  
 2, 
@@ -366,7 +367,7 @@ NULL,
 );
 
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_2_extension_n3', 1, 
+'http_rule_$u{rules_suffix}_Doc_extension_n3', 1, 
 '($u{uri_doc_slash})?(/[^#]*)\\.(ttl)', 
 vector ('par_1', 'par_2', 'par_3'),
 3, 
@@ -380,7 +381,7 @@ NULL,
 );
 
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_2_extension_xml', 1, 
+'http_rule_$u{rules_suffix}_Doc_extension_xml', 1, 
 '($u{uri_doc_slash})?(/[^#]*)\\.(rdf)', 
 vector ('par_1', 'par_2', 'par_3'),
 3, 
@@ -394,12 +395,12 @@ NULL,
 );
 
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_2_extension_json', 1, 
+'http_rule_$u{rules_suffix}_Doc_extension_json', 1, 
 '($u{uri_doc_slash})?(/[^#]*)\\.(json)', 
 vector ('par_1', 'par_2', 'par_3'),
 3, 
-'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20DESCRIBE%%20%%3Chttp%%3A%%2F%%2F$u{domain_name_encoded}%%2F$u{uri_id_encoded}%U%%3E%%20$u{graphs_select_encoded}&format=json', 
-vector ('par_3'), 
+'/sparql?query=define%%20sql%%3Adescribe-mode%%20%%22LOD%%22%%20DESCRIBE%%20%%3Chttp%%3A%%2F%%2F$u{domain_name_encoded}%%2F$u{uri_id_encoded}%U%%3E%%20$u{graphs_select_encoded}&output=application%%2Frdf%%2Bjson', 
+vector ('par_2'), 
 NULL, 
 NULL, 
 2, 
@@ -408,7 +409,7 @@ NULL,
 );
 
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_2_extension_html', 1, 
+'http_rule_$u{rules_suffix}_Doc_extension_html', 1, 
 '($u{uri_doc_slash})?(/[^#]*)\\.(html)', 
 vector ('par_1', 'par_2', 'par_3'),
 3, 
@@ -422,7 +423,7 @@ NULL,
 );
 
 DB.DBA.URLREWRITE_CREATE_REGEX_RULE ( 
-'http_rule_$u{rules_suffix}_2_extension_opac', 1, 
+'http_rule_$u{rules_suffix}_Doc_extension_opac', 1, 
 '($u{uri_doc_slash})?(/[^#]*)\\.(opac)', 
 vector ('par_1', 'par_2', 'par_3'),
 3, 
@@ -434,3 +435,5 @@ NULL,
 303, 
 '' 
 );
+
+DB.DBA.associate_list_ext_rules_to_virtpath ($u{create_virtual_path}, $u{urrl_list_subset}, '$u{rules_suffix_dataset}', '$u{rules_suffix}', '$u{vhost}', '$u{lhost}', '$u{uri_doc_concept_parent}');
