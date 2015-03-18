62ea6ac866c41020de346fb1563c93a6766722a3
idoiamurua <idoia.murua@tecnalia.com>
2015-03-18 12:15:19 +0100

    Remove "id/resource" prefix to generate URIs.

--- aliada/aliada-rdfizer/src/main/java/eu/aliada/rdfizer/Function.java
+++ aliada/aliada-rdfizer/src/main/java/eu/aliada/rdfizer/Function.java
@@ -102,15 +102,17 @@ public class Function {
 			            .replaceAll("[^A-Za-z0-9]", "");
 	}		
 	
-	public String lidoClass(final String value) {
+	public String lidoClass(final Integer id, final String value) {
 		try {
-			final String midx = "/id/resource/";
-			
-			int indexOfIdResource = value.indexOf(midx);
-			if (indexOfIdResource != -1) {
-				int indexOfSlash = value.indexOf("/", (indexOfIdResource + midx.length() + 1));
-				if (indexOfSlash != -1) {
-					return value.substring((indexOfIdResource + midx.length()), indexOfSlash);
+			JobInstance instance = cache.getJobInstance(id);
+			if (instance != null) {
+				final String midx = instance.getNamespace();
+				int indexOfIdResource = value.indexOf(midx);
+				if (indexOfIdResource != -1) {
+					int indexOfSlash = value.indexOf("/", (indexOfIdResource + midx.length() + 1));
+					if (indexOfSlash != -1) {
+						return value.substring((indexOfIdResource + midx.length()), indexOfSlash);
+					}
 				}
 			}
 			return null;
