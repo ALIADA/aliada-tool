aaaa318a163be766a0234d99f45baa0daa114ed7
Andrea Gazzarini <a.gazzarini@gmail.com>
2015-04-22 13:42:12 +0200

    75: RDF-izer: New function for splitting out date expression
    Task-Url: https://github.com/ALIADA/aliada-tool/issues/issue/75

--- aliada/aliada-rdfizer/src/test/java/eu/aliada/rdfizer/FunctionTest.java
+++ aliada/aliada-rdfizer/src/test/java/eu/aliada/rdfizer/FunctionTest.java
@@ -1,6 +1,7 @@
 package eu.aliada.rdfizer;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
@@ -14,6 +15,40 @@ public class FunctionTest {
 
 	private Function cut = new Function();
 
+	@Test
+	public void splitDates_bothDates() {
+		final String [] test = {"1988-1999","1988 - 1999", "1988- 1999", "1988 1999"};
+		final String expectedBirth = "1988";
+		final String expectedDeath = "1999";
+		
+		for (final String dateExpression : test) {
+			assertEquals(dateExpression, expectedBirth, cut.splitDateAndPartialGet(dateExpression, true));			
+			assertEquals(dateExpression, expectedDeath, cut.splitDateAndPartialGet(dateExpression, false));			
+		}
+	}
+	
+	@Test
+	public void splitDates_onlyBirthDates() {
+		final String [] test = {"1988- ","1988 - ", "1988", "1988 ", "d. 1988 "};
+		final String expectedBirth = "1988";
+		
+		for (final String dateExpression : test) {
+			assertEquals(dateExpression, expectedBirth, cut.splitDateAndPartialGet(dateExpression, true));			
+			assertNull(dateExpression, cut.splitDateAndPartialGet(dateExpression, false));			
+		}
+	}
+	
+	@Test
+	public void splitDates_onlyDeathDates() {
+		final String [] test = {"-1999","- 1999"};
+		final String expectedDeath= "1999";
+		
+		for (final String dateExpression : test) {
+			assertEquals(dateExpression, expectedDeath, cut.splitDateAndPartialGet(dateExpression, false));			
+			assertNull(dateExpression, cut.splitDateAndPartialGet(dateExpression, true));			
+		}
+	}
+	
 	@Test
 	public void normalizeWithCopyrightSymbol() {
 		assertEquals("botticellisandro", cut.normalize(" ©  Botticelli©Sandro   "));
